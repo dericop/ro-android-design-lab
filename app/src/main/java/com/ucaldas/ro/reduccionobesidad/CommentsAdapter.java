3696b6f003b3ce60bc1925ba494fda2ba09aeaef
dericop <danielrico.posada@gmail.com>
2017-03-06 15:02:35 -0500

    Actualización de foto en comentario.

--- app/src/main/java/com/ucaldas/ro/reduccionobesidad/CommentsAdapter.java
+++ app/src/main/java/com/ucaldas/ro/reduccionobesidad/CommentsAdapter.java
@@ -1,13 +1,28 @@
 package com.ucaldas.ro.reduccionobesidad;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import java.util.List;
 
@@ -53,14 +68,54 @@ public class CommentsAdapter extends ArrayAdapter<Comment> {
 
         TextView detail = (TextView) convertView.findViewById(R.id.detail);
         TextView userName = (TextView) convertView.findViewById(R.id.titleName);
+        final ImageView avatar = (ImageView) convertView.findViewById(R.id.avatar);
 
-        if(mHome.user != null)
+        if(mHome.user != null){
             userName.setText(mHome.user.getDisplayName());
+            Glide.with(mContext)
+                .load(mHome.user.getPhotoUrl())
+                .asBitmap()
+                .listener(new RequestListener<Uri, Bitmap>() {
+                    @Override
+                    public boolean onException(Exception e, Uri model, Target<Bitmap> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, Uri model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
+
+                        return false;
+                    }
+                })
+                .into(avatar);
+
+        }
 
         detail.setText(com.getDetail());
 
 
-
         return convertView;
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap
+                .getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = pixels;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
