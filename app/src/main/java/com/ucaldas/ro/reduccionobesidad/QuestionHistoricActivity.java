a2d51d58cc31c2c8655c248a495681d5371ef581
dericop <danielrico.posada@gmail.com>
2017-05-25 16:01:35 -0500

    Mejoras vista de ffedback

--- app/src/main/java/com/ucaldas/ro/reduccionobesidad/QuestionHistoricActivity.java
+++ app/src/main/java/com/ucaldas/ro/reduccionobesidad/QuestionHistoricActivity.java
@@ -14,9 +14,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class QuestionHistoricActivity extends AppCompatActivity {
 
@@ -32,6 +34,9 @@ public class QuestionHistoricActivity extends AppCompatActivity {
     private TextView countOfGray;
     private TextView totalQuestions;
 
+    private AtomicInteger greenCount = new AtomicInteger(0);
+    private AtomicInteger redCount = new AtomicInteger(0);
+    private AtomicInteger grayCount = new AtomicInteger(0);
 
 
     @Override
@@ -41,7 +46,7 @@ public class QuestionHistoricActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if(getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
@@ -56,7 +61,7 @@ public class QuestionHistoricActivity extends AppCompatActivity {
         return false;
     }
 
-    private void init(){
+    private void init() {
         questionsList = (ListView) findViewById(R.id.questionsList);
         mQuestions = new LinkedList();
         mQuestionsAdapter = new QuestionsAdapter(getBaseContext(), mQuestions);
@@ -68,7 +73,7 @@ public class QuestionHistoricActivity extends AppCompatActivity {
         totalQuestions = (TextView) findViewById(R.id.totalQuestions);
     }
 
-    private void configureDatabaseAndGetData(){
+    private void configureDatabaseAndGetData() {
         database = FirebaseDatabase.getInstance();
         if (database != null && mHome.user != null) {
             datRef = database.getReference().child("questions");
@@ -80,21 +85,54 @@ public class QuestionHistoricActivity extends AppCompatActivity {
                         HashMap<String, HashMap<String, Object>> map = (HashMap) dataSnapshot.getValue();
                         SortedSet<String> keys = new TreeSet<String>(map.keySet());
 
-                        totalQuestions.setText("/"+keys.size()+"");
+                        totalQuestions.setText("/" + keys.size() + "");
 
                         for (String k : keys) {
                             final Question question = new Question();
-                            question.setTitle(map.get(k).get("title")+"");
-                            question.setId(map.get(k).get("id")+"");
-                            question.setCorrect(map.get(k).get("correct")+"");
-                            question.setResponse1(map.get(k).get("response1")+"");
-                            question.setResponse2(map.get(k).get("response2")+"");
-                            question.setStartDate((long)map.get(k).get("startDate"));
-                            question.setEndDate((long)map.get(k).get("endDate"));
+                            question.setTitle(map.get(k).get("title") + "");
+                            question.setId(map.get(k).get("id") + "");
+                            question.setCorrect(map.get(k).get("correct") + "");
+                            question.setResponse1(map.get(k).get("response1") + "");
+                            question.setResponse2(map.get(k).get("response2") + "");
+                            question.setStartDate((long) map.get(k).get("startDate"));
+                            question.setEndDate((long) map.get(k).get("endDate"));
 
                             mQuestions.add(question);
                             mQuestionsAdapter.notifyDataSetChanged();
 
+                            FirebaseDatabase.getInstance().getReference().child("questions-user")
+                                    .child(mHome.user.getUid()).orderByChild("question").equalTo(question.getId())
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.getValue() != null) {
+                                                HashMap<String, HashMap> map = (HashMap) dataSnapshot.getValue();
+                                                Iterator it = map.keySet().iterator();
+                                                HashMap response = map.get(it.next()+"");
+
+                                                Log.v("Questions ",response.get("isCorrect")+"");
+
+                                                if((boolean)response.get("isCorrect")){
+                                                    greenCount.incrementAndGet();
+                                                    countOfGreen.setText(greenCount.get()+"");
+                                                }else{
+                                                    redCount.incrementAndGet();
+                                                    countOfRed.setText(redCount.get()+"");
+                                                }
+                                            }else{
+                                                grayCount.incrementAndGet();
+                                                countOfGray.setText(grayCount.get()+"");
+                                            }
+
+
+
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+
+                                        }
+                                    });
                         }
                     }
                 }
